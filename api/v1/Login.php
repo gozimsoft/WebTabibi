fe7254ff9295de0c358aa65f8010447f9cd1dd65
Amar Gozim <68405276+gozimsoft@users.noreply.github.com>
2025-07-17 19:43:39 +0100

    e

--- api/v1/Login.php
+++ api/v1/Login.php
@@ -8,7 +8,7 @@ $data = json_decode(file_get_contents("php://input"), true);
 $username = $data["username"] ?? "";
 $password = $data["password"] ?? "";
 
-  if ($username === "" || $password === "")  {
+  if ($username === "" || $password === "") {
     echo json_encode([
         "status" => "fail",
         "message" => "Username or password is missing"
--- api/v1/download_patient.php
+++ api/v1/download_patient.php
@@ -3,7 +3,7 @@ header("Content-Type: application/json");
 require_once("config.php");
 require_once("auth.php");
 
- 
+
 $headers = getallheaders();
 $authHeader = $headers['Authorization'] ?? '';
 $token = str_replace('Bearer ', '', $authHeader);
@@ -20,32 +20,25 @@ $data = json_decode(file_get_contents("php://input"), true);
 $phone = $data['phone'] ?? '';
 $email = $data['email'] ?? '';
 
-  
- 
+
+
 
 // الحصول على Patient_id من رقم الهاتف
 $stmt = $pdo->prepare("SELECT * FROM Patients WHERE ( PhoneValidation = 1 AND Phone LIKE :phone) 
  or ( EmailValidation = 1 AND Email LIKE :email)   LIMIT 1");
-$stmt->execute([':phone' => $phone ,
-':email' => $email
-]  );
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt->execute([
+    ':phone' => $phone,
+    ':email' => $email
+]);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-  echo json_encode(['status' => 'fail', 'message' => $phone]);
-    exit;
-
-
-if (!$row) {
-    echo json_encode(['status' => 'fail', 'message' => 'Patient not found']);
-    exit;
+if (! empty($rows)) {
+    echo json_encode([
+        'status' => 'success',
+        'data' => $rows
+    ]);
+} else {
+    echo json_encode(['status' => 'fail', 'message' => 'no data']);
 }
-
-$patient_id = $row['ID'];
-
-
-echo json_encode(['status' => 'success', 'message' => 'Apointement saved']);
-
- 
 
 ?>
